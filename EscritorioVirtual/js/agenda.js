3970db39f24c913edf7ef1d53052f3263d59de12
Mister-Mario <102600139+Mister-Mario@users.noreply.github.com>
2023-12-14 17:48:36 +0100

    Add files via upload

---
+++ EscritorioVirtual/js/agenda.js
@@ -0,0 +1,57 @@
+"use strict";
+class Agenda{
+    constructor(){
+        this.last_api_call = null;
+        this.last_api_result = null;
+    }
+
+    getCarreras(){
+        var agenda2 = this;
+        var callAgain = false;
+        if(this.last_api_call !== null){
+            var now = new Date();
+            callAgain = (now - this.last_api_call > 10 * 60 * 1000);
+        }
+        else
+            callAgain = true;
+
+        if(callAgain){
+            this.last_api_call = new Date();
+            $.ajax({
+                    dataType: "xml",
+                    url : "http://ergast.com/api/f1/current",
+                    method : 'GET',                          
+                    }).done(function(data){
+                        agenda2.saveData(data);
+                        agenda2.paintInformation();
+                    });
+        }
+        else{
+            this.paintInformation();
+        }
+             
+    }
+
+    saveData(data){
+        this.last_api_result = data;    
+    }
+
+    paintInformation(){
+        $("main").empty();
+
+        $("main").append("<section data-type = 'carreras'></section>");
+        $("Race", this.last_api_result).each((index, race) =>{
+            $("section[data-type='carreras']").append("<section data-type = 'carrera' data-i ='" + index + "'></section>");
+            $("section[data-i = '"+ index +"'")
+                .append("<p>"+  $("RaceName",race).text() +"</p>")
+                .append("<p>"+  $("CircuitName",race).text() +"</p>")
+                .append("<p> Fecha: "+  $("Date",race)[0].textContent +"</p>")
+                .append("<p> Hora: "+  $("Time",race)[0].textContent.split('Z')[0] +"</p>")
+                .append("<p>Latitud: "+  $("Location",race).attr("lat") +"</p>")
+                .append("<p>Longitud: " +  $("Location",race).attr("long") +"</p>");
+})
+    }
+}
+
+
+var agenda = new Agenda();
---
+++ EscritorioVirtual/js/sudoku.js
@@ -0,0 +1,169 @@
+"use strict";
+class Sudoku{
+    constructor (tablero){
+        this.tableroTexto = tablero;
+        this.maxFilas = 9;
+        this.maxCols = 9;
+        this.tablero = new Array(this.maxFilas);
+        for(let i = 0; i < this.maxCols; i++)
+            this.tablero[i] = new Array(this.maxCols);
+        this.start();
+    }
+
+    start(){
+        var counter = 0;
+        for(let i = 0; i < this.maxFilas; i++)
+            for(let j = 0; j < this.maxCols; j++){
+                this.tablero[i][j] = this.tableroTexto[counter] === '.' ? 0 : this.tableroTexto[counter];
+                counter += 1;
+            }
+                
+    }
+
+    setTablero(tablero){
+        this.tableroTexto = tablero;
+        this.start();
+    }
+
+    createStructure(){
+        var main = document.querySelector("main");
+        var sections = new Array(this.maxFilas);
+        for(let i = 0; i < this.maxFilas; i++){
+            sections[i] = document.createElement("section");
+            sections[i].setAttribute("data-name", "game");
+        }
+        var p = null;
+        for(let i = 0; i < this.maxFilas; i++)
+            for(let j = 0; j < this.maxCols; j++){
+                p = document.createElement("p");
+                if(this.tablero[i][j] === 0){
+                    p.setAttribute("data-state", "");
+                    p.onclick = this.addClicked.bind(p, this);
+                }
+                else{
+                    p.setAttribute("data-state", "blocked");
+                    p.textContent = this.tablero[i][j];
+                }
+                var subGridRowStart = (i - i%3);
+                var subGridColStart = (j - j%3);
+                var subGrid =  subGridRowStart + Math.floor(subGridColStart/3);
+                sections[subGrid].appendChild(p);
+            }
+        
+        for(let i = 0; i < this.maxFilas; i++){
+            main.appendChild(sections[i]);
+        }
+    }
+
+    paintSudoku(){
+        this.createStructure();
+    }
+
+    addClicked(sudoku){
+        if(sudoku.isAnyCellClicked())
+            sudoku.getClickedCellRowCol(sudoku.getCellsOrderedByRow())[0].setAttribute("data-state", "");
+        this.setAttribute("data-state", "clicked");
+    }
+
+    checkKey(sudoku, event){
+        if (event.key >= '0' && event.key <= '9') {
+            if(sudoku.isAnyCellClicked())
+                sudoku.introduceNumber(event.key);
+            else{
+                alert("Por favor elige una casilla para rellenar");
+            }
+                
+        }
+    }
+
+    isAnyCellClicked(){
+        var cells = document.querySelectorAll("p");
+        for(let i = 0; i < this.maxFilas; i++)
+            for(let j = 0; j < this.maxCols; j++)
+                if(cells[(i * 9) + j].getAttribute("data-state") === "clicked")
+                    return true;
+        return false;
+    }
+
+    getClickedCellRowCol(cells){
+        for(let i = 0; i < this.maxFilas; i++)
+            for(let j = 0; j < this.maxCols; j++)
+                if(cells[i][j].getAttribute("data-state") === "clicked")
+                    return [cells[i][j], i, j];
+        return null;
+    }
+
+    introduceNumber(number){
+        var cells = this.getCellsOrderedByRow();
+        var cellInformation = this.getClickedCellRowCol(cells);
+        if(!this.isNumberInCol(cells, number, cellInformation[2])
+            && !this.isNumberInRow(cells, number, cellInformation[1])
+            && !this.isNumberInSubGrid(cells, number, cellInformation[1], cellInformation[2])){
+                cellInformation[0].onclick = null;
+                cellInformation[0].setAttribute("data-state", "correct");
+                cellInformation[0].textContent = number;
+                if(this.isEveryCellFilled(cells))
+                    alert("Felicidades el sudoku esta completo");
+            }
+    }
+
+    isEveryCellFilled(cells){
+        var flag = true;
+        for(let i = 0; i < this.maxFilas; i++)
+            for(let j = 0; j < this.maxCols; j++)
+                flag = flag && (cells[i][j].getAttribute("data-state") === "blocked" 
+                    || cells[i][j].getAttribute("data-state") === "correct");
+                    
+        return flag;
+    }
+
+    isNumberInCol(cells, number, col){
+        for(let i = 0; i < this.maxFilas; i++){
+            if(cells[i][col].textContent === number)
+                return true;
+        }
+        return false;
+    }
+
+    isNumberInRow(cells, number, row){
+        for(let i = 0; i < this.maxFilas; i++){
+            if(cells[row][i].textContent === number)
+                return true;
+        }
+        return false;
+    }
+
+    isNumberInSubGrid(cells, number, row, col){
+        var subGridRowStart = (row - row%3);
+        var subGridColStart = (col - col%3);
+        for(let i = subGridRowStart; i < subGridRowStart + 3; i++)
+            for(let j = subGridColStart; j < subGridColStart + 3; j++)
+                if(cells[i][j].textContent === number)
+                    return true;
+        return false;
+    }
+
+    getCellsOrderedByRow(){
+        var cells = document.querySelectorAll("p");
+        var retCells = new Array(this.maxFilas);
+        for(let i = 0; i < this.maxCols; i++)
+            retCells[i] = new Array(this.maxCols);
+        var iModifier = 0;
+        for(let i = 0; i < this.maxFilas; i++){
+            if(i%3 === 0 && i !== 0)
+                iModifier += 18;
+            var iModified = (i * 3) + iModifier;
+            var jModifier = 0;
+            for(let j = 0; j < this.maxCols; j++){
+                if(j%3 === 0 && j !== 0)
+                    jModifier += 6;
+                var jModified = j + jModifier;
+                retCells[i][j] = cells[(iModified) + jModified];
+                }
+        }
+
+        return retCells;
+    }
+}
+
+var sudoku = new Sudoku("3.4.69.5....27...49.2..4....2..85.198.9...2.551.39..6....8..5.32...46....4.75.9.6");
